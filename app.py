0c4a39988f755e4aa433f84b1ab8465a7b2fea94
ahsan123322 <96461644+ahsan123322@users.noreply.github.com>
2023-03-31 20:32:31 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import requests
+from PIL import Image
+import io
+
+API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.content
+
+def app():
+    # Set the app title
+    st.title("Image Generator")
+
+    # Add a text input field for the user to enter text
+    text_input = st.text_input("Enter text")
+
+    # Add a button to submit the text
+    if st.button("Generate"):
+        # Call the query function with the user input
+        image_bytes = query({
+            "inputs": text_input,
+        })
+
+        # Display the generated image
+        image = Image.open(io.BytesIO(image_bytes))
+        st.image(image, caption="Generated Image")
+if __name__ == "__main__":
+    app()
